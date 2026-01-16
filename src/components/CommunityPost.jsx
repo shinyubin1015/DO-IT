e84b11e1864a4cfb71666c3faefd15b8cbcb8697
honeybutter <honeybutter900@gmail.com>
2026-01-17 04:43:32 +0900

    Add CommunityPost component for displaying community posts

---
+++ src/components/CommunityPost.jsx
@@ -0,0 +1,15 @@
+function CommunityPost() {
+    return (
+        <div className="Community-post">
+            <a href="" className="Community-post-content">
+                <h4>----커뮤니티 글 제목----</h4>
+                <div className="Community-content">
+                    <span className="view">319</span>
+                    <span className="date">2008-03-19</span>
+                    <span className="Comment">7</span>
+                </div>
+            </a>
+        </div>
+    );
+}
+export default CommunityPost;
--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,8 +1,51 @@
+import React from "react";
+import CommunityPost from "../components/CommunityPost";
+
 function Community() {
     return (
-        <div className="Community-header">
-            <input type="text" value='검색어를 입력해세요' />
-        </div>
+        <section className="Community">
+            <div className="Community-header">
+                <div className="search">
+                    <input type="text" placeholder="검색어를 입력해세요" />
+                    <button><img src="./images/icon/search1.png" alt="검색"/></button>
+                </div>
+            </div>
+            <div className="Community-main">
+                <div className="Community-main-title">
+                    <div className="Community-main-title-content">
+                        <h2>제목</h2>
+                        <div className="Community-title-content">
+                            <span className="view">조회수</span>
+                            <span className="date">등록일</span>
+                            <span className="comment">댓글</span>
+                        </div>
+                    </div>
+                </div>
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+                <CommunityPost />
+            </div>
+            <footer className="Community-footer">
+                <div className="page-number">
+                    <button className="prev">←</button>
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <span>...</span>
+                    <button>23</button>
+                    <button className="next">→</button>
+                </div>
+            </footer>
+        </section>
         
     );
 }
