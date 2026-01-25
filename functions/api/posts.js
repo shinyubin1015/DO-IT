41b74b6f632edbb57a8c07ccbb3b09c51d963de9
yuna706 <riyu6027@naver.com>
2026-01-25 16:54:29 +0900

    feat: d1 database

--- functions/api/posts.js
+++ functions/api/posts.js
@@ -1,5 +1,7 @@
 import posts from './post/data'
 
-export function onRequestGet(){
+export async function onRequestGet({env}){
+    const result = await env.D1_DB.prepare('SELECT * FROM USER').all();
+    console.log(result.results);
     return Response.json(posts)
 }
--- worker/index.js
+++ worker/index.js
@@ -2,17 +2,20 @@ import * as posts from "../functions/api/posts";
 import * as postById from "../functions/api/post/[[id]]";
 
 export default {
-  fetch(request, env) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === ("/api/posts")) {
-      return posts.onRequestGet();
+    if (url.pathname === "/api/posts") {
+      // /api/posts 경로로 요청이 들어오면 posts.onRequestGet 호출
+      return posts.onRequestGet({ env });
     } else if (url.pathname.startsWith("/api/post/")) {
+      // /api/post/:id 경로로 요청이 들어오면 ID 추출하여 postById.onRequestGet 호출
       const id = url.pathname.replace(/^\/api\/post\/+/, '');
       const context = { params: { id } };
-      return postById.onRequestGet(context);
+      return postById.onRequestGet({ env, params: context.params });
     }
 
-		return new Response(null, { status: 404 });
-  },
+    // 위 두 조건에 맞지 않으면 404 응답
+    return new Response(null, { status: 404 });
+  }
 }
